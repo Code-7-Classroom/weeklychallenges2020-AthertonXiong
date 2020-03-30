b024f27b045546fdad941f332f8695d762240697
AthertonXiong <xatherton@hotmail.com>
2020-03-30 19:40:30 -0400

    Here is my unfinished work but still something

---
+++ eCommerce Project/Javascript/index.js
@@ -0,0 +1,23 @@
+var slideInterval = 3;
+function getFigures() {
+    return document.getElementById('slideshow').getElementsByTagName('figure');
+}
+function moveForward() {
+    var pointer = 0;
+    var figures = getFigures();
+    for (var i = 0; i < figures.length; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = 'hidden';
+            pointer = i;
+        }
+    }
+    if (++pointer == figures.length) {
+        pointer = 0;
+    }
+    figures[pointer].className = 'visible';
+    setTimeout(moveForward, slideInterval);
+}
+function startPlayback() {
+    setTimeout(moveForward, slideInterval);
+}
+startPlayback();
